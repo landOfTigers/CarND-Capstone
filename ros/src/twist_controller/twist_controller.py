14b493d130e32b6e670ae331c069575d6acda1b0
Hanna Becker <hanna.e.becker@gmail.com>
2019-04-30 21:28:51 +0200

    increase maximum allowed throttle to 1

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -15,7 +15,7 @@ class Controller(object):
         ki = 0.1
         kd = 0.0
         min_throttle = 0.0
-        max_throttle = 0.2
+        max_throttle = 1.0
         self.throttle_controller = PID(kp, ki, kd, min_throttle, max_throttle)
 
         tau = 0.5  # 1/(2*pi*tau) = cutoff frequency
