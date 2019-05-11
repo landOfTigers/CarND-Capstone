bed9fd645c6521ac52c8b591e99f6c8f15fe75ee
Hanna Becker <hanna.e.becker@gmail.com>
2019-05-11 13:02:15 +0200

    train for 4 epochs

--- ros/src/tl_detector/light_classification/model.py
+++ ros/src/tl_detector/light_classification/model.py
@@ -31,7 +31,7 @@ model.summary()
 # 2: compile and fit the model
 model.compile('adam', 'categorical_crossentropy', ['accuracy'])
 x_train, y_train = create_samples_from_log()
-history = model.fit(x_train, y_train, epochs=3, batch_size=16, validation_split=0.2)
+history = model.fit(x_train, y_train, epochs=4, batch_size=16, shuffle=True, validation_split=0.2)
 
 # 3: save history to file
 timestamp = datetime.datetime.now().strftime('%Y_%m_%d__%H:%M')
