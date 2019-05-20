7c4606dae27402cc04ee07c17a46eee73afadd86
Hanna Becker <hanna.e.becker@gmail.com>
2019-05-20 17:10:10 +0200

    remove unimplemented obstacle_cb

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -36,8 +36,6 @@ class WaypointUpdater(object):
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
         rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
 
-        # TODO: Add a subscriber for /obstacle_waypoint below
-
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         self.pose = None
@@ -126,10 +124,6 @@ class WaypointUpdater(object):
 
     def traffic_cb(self, msg):
         self.stopline_waypoint_idx = msg.data
-
-    def obstacle_cb(self, msg):
-        # TODO: Callback for /obstacle_waypoint message. We will implement it later
-        pass
 
     @staticmethod
     def distance(waypoints, wp1, wp2):
